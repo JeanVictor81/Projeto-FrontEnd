68d5487e1e42f2358521a6ef3b6157aa3b2dff7e
LuizNery-2 <62023495+LuizNery-2@users.noreply.github.com>
2023-05-27 21:40:37 -0300

    Carrossel e inicio do chatbot

--- js/script.js
+++ js/script.js
@@ -1,37 +1,15 @@
-const controls = document.querySelectorAll(".control");
-let currentItem = 0;
-const items = document.querySelectorAll(".item");
-const maxItems = items.length;
-
-controls.forEach((control) => {
-  control.addEventListener("click", (e) => {
-    isLeft = e.target.classList.contains("arrow-left");
-
-    if (isLeft) {
-      currentItem -= 1;
-    } else {
-      currentItem += 1;
-    }
-
-    if (currentItem >= maxItems) {
-      currentItem = 0;
-    }
-
-    if (currentItem < 0) {
-      currentItem = maxItems - 1;
-    }
-
-    items.forEach((item) => item.classList.remove("current-item"));
-
-    items[currentItem].scrollIntoView({
-      behavior: "smooth",
-      inline: "center"
-    });
-
-    items[currentItem].classList.add("current-item");
-  });
-});
   
+const menuCTA = document.querySelector('.navCTA');
+const menuPesquisa = document.querySelector('.menuPesquisa');
+const menuPrincipal = document.querySelector('.menuPrincipal');
+const imgCarrossel = document.querySelector('.imagensCarrossel');
+const btnCarrossel = document.querySelectorAll('.carrosselCheckBox input');
+const productsButtonsList = document.querySelectorAll(".btnProduto");
+const listProdutcs = document.querySelectorAll(".produto")
+const buttons = menuPrincipal.querySelectorAll('button');
+let rodadasCarrossel;
+
+
 function irParaHome() {
   if(document.title == 'FLOWERS')
   {
@@ -43,7 +21,6 @@ function irParaHome() {
   }
   
 }
-
 function irParaLogin() {
     if (document.title == 'FLOWERS') {
       window.location.href = "html/Login.html";
@@ -54,7 +31,6 @@ function irParaLogin() {
     }
     
 }
-
 function irParaCadastro() 
 {
   if (document.title == 'FLOWERS') {
@@ -66,25 +42,117 @@ function irParaCadastro()
   }
   
 }
+function removerCTAMenu() {
+  
+  
+  menuCTA.style.display = 'none';
+  menuPesquisa.style.top = '0';
 
-const listProdutcs = document.querySelectorAll(".produto")
-listProdutcs.forEach((element) => {
-  element.addEventListener("mouseover",()=>{
+  menuPrincipal.classList.add('menuPrincipalPag')
+  
 
-    element.childNodes[7].style.display = 'flex';
-    element.childNodes[7].classList.add("active");
-  })
-  element.addEventListener("mouseout",()=>{
-        
-       
-    element.childNodes[7].classList.remove("active");
+  
+
+}
+function adicionarCTAMenu(){
+  
+  
+  menuCTA.style.display = 'flex';
+  menuPesquisa.style.top = '5vh';
+  menuPrincipal.classList.remove('menuPrincipalPag') 
  
-    
+
+     
+
+}
+function manipularRolagem() {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  var limite = 500; 
+
+  
+    if (scrollTop > limite) {
+      removerCTAMenu();
+    } 
+    else {
+      adicionarCTAMenu();
+    }
+  
+
+}
+function animarProduto() {
+  listProdutcs.forEach((element) => {
+    element.addEventListener("mouseover",()=>{
+  
+      element.childNodes[7].style.display = 'flex';
+      element.childNodes[7].classList.add("active");
+    })
+    element.addEventListener("mouseout",()=>{
+          
+         
+      element.childNodes[7].classList.remove("active");
+   
+      
+    })
   })
+  
+  productsButtonsList.forEach(element => {
+        element.setAttribute("data-btnTrasition","btnBonito");
+  });
+  
+}
+function animarBtnMenu(){
+  buttons.forEach(currentButton => {
+    currentButton.addEventListener("click", () => {
+     buttons.forEach(otherButton => {
+        if (otherButton.classList.contains('botoesMenuAtual')) 
+        {
+            otherButton.classList.remove('botoesMenuAtual');
+            otherButton.classList.add('botoesMenu');
+        }
+        currentButton.classList.add('botoesMenuAtual');
+        
+
+     })
+  })
+
 })
-const productsButtonsList = document.querySelectorAll(".btnProduto");
-productsButtonsList.forEach(element => {
-      element.setAttribute("data-btnTrasition","btnBonito");
-});
+}
+
+function automatizarCarrossel(i) {
+  rodadasCarrossel = setInterval(() => {
+    if (i == btnCarrossel.length) {
+      i = 0;
+    }
+    imgCarrossel.style.transform = `translateX(${-100 * i}%)`;
+    i++;
+  }, 3000);
+}
+
+function reiniciarAutomatizacao(i) {
+  clearInterval(rodadasCarrossel);
+  setTimeout(() => {
+    automatizarCarrossel(i);
+  }, 3000);
+}
+
+function navegarCarrossel() {
+  for (let i = 0; i < btnCarrossel.length; i++) {
+    btnCarrossel[i].addEventListener('click', () => {
+      imgCarrossel.style.transform = `translateX(${-100 * i}%)`;
+      clearInterval(rodadasCarrossel);
+      setTimeout(reiniciarAutomatizacao(i),3000) 
+    });
+  }
+}
+navegarCarrossel();
+automatizarCarrossel(0);
+animarBtnMenu()
+animarProduto();
+
+window.addEventListener('scroll', manipularRolagem)
+
+
+
+
 
 
